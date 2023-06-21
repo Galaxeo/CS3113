8b3f8ce7a0e6dae728ddfd1ea05e70fe09fc6a9a
Galaxeo <hwjustincheok@gmail.com>
2023-06-20 20:13:26 -0400

    feat: working demo

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,4 +1,4 @@
-/**
+ï»¿/**
 * Author: Justin Cheok
 * Assignment: Pong Clone
 * Date due: 2023-06-23, 11:59pm
@@ -47,20 +47,9 @@ const char paddle1_SPRITE[] = "evan.png";
 const char paddle2_SPRITE[] = "mage.png";
 const char ball_SPRITE[] = "slime.png";
 
-const float ROT_SPEED = 100.0f;
-
-const glm::vec3 paddle1_INIT_POS = glm::vec3(4.0f, 1.0f, 0.0f),
-paddle1_INIT_SCA = glm::vec3(1.0f, 1.0f, 0.0f);
-
-const glm::vec3 paddle2_INIT_POS = glm::vec3(-4.0f, -1.5f, 0.0f),
-paddle2_INIT_SCA = glm::vec3(1.0f, 1.0f, 0.0f);
-
-const glm::vec3 ball_INIT_POS = glm::vec3(0.0f, 0.0f, 0.0f),
-ball_INIT_SCA = glm::vec3(1.0f, 1.0f, 0.0f);
-
 const int NUMBER_OF_TEXTURES = 1;
-const GLint LEVEL_OF_DETAIL = 0,
-TEXTURE_BORDER = 0;
+const GLint LEVEL_OF_DETAIL = 0;
+const GLint TEXTURE_BORDER = 0;
 
 const float MILLISECONDS_IN_SECOND = 1000.0;
 
@@ -82,23 +71,27 @@ g_paddle2_model_matrix,
 g_ball_model_matrix,
 g_projection_matrix;
 
-float g_previous_ticks = 0.0f;
-float g_rot_angle = 0.0f;
-float g_speed = 1.0f;
+float g_paddle_speed = 3.0f,
+g_ball_speed = 2.5f,
+g_previous_ticks = 0.0f;
 
-glm::vec3 g_paddle1_movement = glm::vec3(0.0f, 0.0f, 0.0f),
-g_paddle1_position = glm::vec3(0.0f, 0.0f, 0.0f),
+bool start = true;
+
+glm::vec3 g_paddle1_position = glm::vec3(-4.2f, 0.0f, 0.0f),
+g_paddle1_movement = glm::vec3(0.0f, 0.0f, 0.0f),
+
+g_paddle2_position = glm::vec3(4.2f, 0.0f, 0.0f),
 g_paddle2_movement = glm::vec3(0.0f, 0.0f, 0.0f),
-g_paddle2_position = glm::vec3(0.0f, 0.0f, 0.0f),
-g_ball_movement = glm::vec3(0.0f, 0.0f, 0.0f),
-g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f),
 
-g_paddle1_scale = glm::vec3(1.0f, 1.0f, 0.0f),
-g_paddle1_growth = glm::vec3(0.0f, 0.0f, 0.0f),
-g_paddle2_scale = glm::vec3(1.0f, 1.0f, 0.0f),
-g_paddle2_growth = glm::vec3(0.0f, 0.0f, 0.0f),
-g_ball_scale = glm::vec3(1.0f, 1.0f, 0.0f),
-g_ball_growth = glm::vec3(0.0f, 0.0f, 0.0f);
+g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f),
+g_ball_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+
+// Calculating distance between two objects with Pythagorean
+float distance(glm::vec3& position_a, glm::vec3& position_b)
+{
+    return sqrt(pow(position_b.x - position_a.x, 2) + pow(position_b.y - position_a.y, 2));
+}
 
 GLuint load_texture(const char* filepath)
 {
@@ -124,11 +117,11 @@ GLuint load_texture(const char* filepath)
     return textureID;
 }
 
-
 void initialise()
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("User input exercise",
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+
+    g_display_window = SDL_CreateWindow("Pong Clone",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL);
@@ -144,46 +137,40 @@ void initialise()
     g_view_matrix = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
-    // ———————————————— paddle1 ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” paddle1 â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
+
     g_paddle1_program.Load(V_SHADER_PATH, F_SHADER_PATH);
 
     g_paddle1_model_matrix = glm::mat4(1.0f);
-    g_paddle1_model_matrix = glm::translate(g_paddle1_model_matrix, paddle1_INIT_POS);
-    g_paddle1_model_matrix = glm::scale(g_paddle1_model_matrix, paddle1_INIT_SCA);
 
     g_paddle1_program.SetProjectionMatrix(g_projection_matrix);
     g_paddle1_program.SetViewMatrix(g_view_matrix);
-
     glUseProgram(g_paddle1_program.programID);
     g_paddle1_texture_id = load_texture(paddle1_SPRITE);
 
-    // ———————————————— paddle2 ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” paddle2 â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     g_paddle2_program.Load(V_SHADER_PATH, F_SHADER_PATH);
 
     g_paddle2_model_matrix = glm::mat4(1.0f);
-    g_paddle2_model_matrix = glm::translate(g_paddle2_model_matrix, paddle2_INIT_POS);
-    g_paddle2_model_matrix = glm::scale(g_paddle2_model_matrix, paddle2_INIT_SCA);
 
     g_paddle2_program.SetProjectionMatrix(g_projection_matrix);
     g_paddle2_program.SetViewMatrix(g_view_matrix);
-
     glUseProgram(g_paddle2_program.programID);
     g_paddle2_texture_id = load_texture(paddle2_SPRITE);
 
-    // ———————————————— ball ———————————————— //
+
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” ball â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     g_ball_program.Load(V_SHADER_PATH, F_SHADER_PATH);
 
     g_ball_model_matrix = glm::mat4(1.0f);
-    g_ball_model_matrix = glm::translate(g_ball_model_matrix, ball_INIT_POS);
-    g_ball_model_matrix = glm::scale(g_ball_model_matrix, ball_INIT_SCA);
 
     g_ball_program.SetProjectionMatrix(g_projection_matrix);
     g_ball_program.SetViewMatrix(g_view_matrix);
-
     glUseProgram(g_ball_program.programID);
     g_ball_texture_id = load_texture(ball_SPRITE);
 
-    // ———————————————— GENERAL ———————————————— //
+
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” GENERAL â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -214,11 +201,11 @@ void process_input()
     }
     const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
-    if (key_state[SDL_SCANCODE_UP])
+    if (key_state[SDL_SCANCODE_W])
     {
         g_paddle1_movement.y = 1.0f;
     }
-    else if (key_state[SDL_SCANCODE_DOWN])
+    else if (key_state[SDL_SCANCODE_S])
     {
         g_paddle1_movement.y = -1.0f;
     }
@@ -229,22 +216,13 @@ void process_input()
         g_paddle2_movement = glm::normalize(g_paddle2_movement);
     }
 
-    if (key_state[SDL_SCANCODE_W])
+    if (key_state[SDL_SCANCODE_UP])
     {
         g_paddle2_movement.y = 1.0f;
     }
-    else if (key_state[SDL_SCANCODE_S])
+    else if (key_state[SDL_SCANCODE_DOWN])
     {
         g_paddle2_movement.y = -1.0f;
-    }
-}
-
-bool collisionCheck(float x_dist, float y_dist) {
-    if (x_dist < 0 && y_dist < 0) {
-        return true;
-    }
-    else {
-        return false;
     }
 }
 float randomY() {
@@ -252,31 +230,53 @@ float randomY() {
     //Randomize Y-direction for a decimal between 0-1
     return randomInt > 50 ? (rand() % 100 + static_cast<float>(1)) / 100 : -((rand() % 100 + static_cast<float>(1)) / 100);
 }
-bool start = true;
 void update()
 {
     if (start) {
-        g_ball_movement.x = 1.0f;
-        g_ball_movement.y = randomY();
+        g_ball_movement.x = 1.0;
+        // g_ball_movement.y = randomY();
         start = false;
     }
-
-    float collision_factor = 0.07f;
-
+    /* Copied from in-class exercise, did not end up working. Removed everything to do with scaling and made
+    * game purely based on x and y distance (Pythagorean coded in beginning)
     float pad1_x_distance = fabs(g_paddle1_position.x - g_ball_position.x) - ((paddle1_INIT_SCA.x * collision_factor + ball_INIT_SCA.x * collision_factor) / 2.0f);
     float pad1_y_distance = fabs(g_paddle1_position.y - g_ball_position.y) - ((paddle1_INIT_SCA.y * collision_factor + ball_INIT_SCA.y * collision_factor) / 2.0f);
 
     float pad2_x_distance = fabs(g_paddle2_position.x - g_ball_position.x) - ((paddle2_INIT_SCA.x * collision_factor + ball_INIT_SCA.x * collision_factor) / 2.0f);
     float pad2_y_distance = fabs(g_paddle2_position.y - g_ball_position.y) - ((paddle2_INIT_SCA.y * collision_factor + ball_INIT_SCA.y * collision_factor) / 2.0f);
 
-    // ———————————————— DELTA TIME CALCULATIONS ———————————————— //
+    float collision_factor = 0.07f;
+    */
+    
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” COLLISIONS â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
+    if ((g_ball_position.x <= -4.4f) || (g_ball_position.x >= 4.4f)) { // If ball is out
+        g_game_is_running = false;
+    }
+    if (distance(g_ball_position, g_paddle1_position) < 0.75f) { // Left player collision
+        g_ball_movement.x = 1.0f;
+        g_ball_movement.y = randomY();
+    }
+    else if (distance(g_ball_position, g_paddle2_position) < 0.75f) { // Right player collision
+        g_ball_movement.x = -1.0f;
+        g_ball_movement.y = randomY();
+    }
+    // Top & bottom barrier collision
+    if (g_ball_position.y >= 3.75f) {
+        g_ball_movement.y = -1.0f;
+    }
+    else if (g_ball_position.y <= -3.75f) {
+        g_ball_movement.y = 1.0f;
+    }
+
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” DELTA TIME CALCULATIONS â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
 
     if (g_ball_position.x > 4.4f) {
         g_ball_movement.x = -1.0f;
-    }else if (g_ball_position.x < -4.4f) {
+    }
+    else if (g_ball_position.x < -4.4f) {
         g_ball_movement.x = 1.0f;
     }
     if (g_ball_position.y > 3.3f) {
@@ -285,43 +285,29 @@ void update()
     else if (g_ball_position.y < -3.3f) {
         g_ball_movement.y = -g_ball_movement.y;
     }
-    if (pad1_x_distance < 0 && pad1_y_distance < 0) {
-        g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
-    }else if (collisionCheck(pad2_x_distance, pad2_y_distance)) {
-        g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
-    }
 
-    // ———————————————— RESETTING MODEL MATRIX ———————————————— //
+    g_paddle1_position += g_paddle1_movement * delta_time * g_paddle_speed;
+    g_paddle2_position += g_paddle2_movement * delta_time * g_paddle_speed;
+
+    g_ball_position += g_ball_movement * delta_time * g_ball_speed;
+
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” RESETTING MODEL MATRIX & TRANSLATIONS â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     g_paddle1_model_matrix = glm::mat4(1.0f);
-    g_paddle1_model_matrix = glm::translate(g_paddle1_model_matrix, paddle1_INIT_POS);
-
-    g_paddle2_model_matrix = glm::mat4(1.0f);
-    g_paddle2_model_matrix = glm::translate(g_paddle2_model_matrix, paddle2_INIT_POS);
-
-    g_ball_model_matrix = glm::mat4(1.0f);
-    g_ball_model_matrix = glm::translate(g_ball_model_matrix, ball_INIT_POS);
-
-    // ———————————————— TRANSLATIONS ———————————————— //
-    g_paddle1_position += g_paddle1_movement * g_speed * delta_time;
     g_paddle1_model_matrix = glm::translate(g_paddle1_model_matrix, g_paddle1_position);
     g_paddle1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
-    g_paddle2_position += g_paddle2_movement * g_speed * delta_time;
+    g_paddle2_model_matrix = glm::mat4(1.0f);
     g_paddle2_model_matrix = glm::translate(g_paddle2_model_matrix, g_paddle2_position);
     g_paddle2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
-    g_ball_position += g_ball_movement * g_speed * delta_time;
+    g_ball_model_matrix = glm::mat4(1.0f);
     g_ball_model_matrix = glm::translate(g_ball_model_matrix, g_ball_position);
-
-    // ———————————————— ROTATIONS ———————————————— //
-    //g_rot_angle += ROT_SPEED * delta_time;
-    //g_paddle1_model_matrix = glm::rotate(g_paddle1_model_matrix, glm::radians(g_rot_angle), glm::vec3(0.0f, 1.0f, 0.0f));
 }
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // ———————————————— paddle1 ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” paddle1 â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     float paddle1_vertices[] = {
         -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
         -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f
@@ -345,7 +331,7 @@ void render() {
     glDisableVertexAttribArray(g_paddle1_program.positionAttribute);
     glDisableVertexAttribArray(g_paddle1_program.texCoordAttribute);
 
-    // ———————————————— paddle2 ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” paddle2 â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     float paddle2_vertices[] = {
         -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
         -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f
@@ -369,7 +355,7 @@ void render() {
     glDisableVertexAttribArray(g_paddle2_program.positionAttribute);
     glDisableVertexAttribArray(g_paddle2_program.texCoordAttribute);
 
-    // ———————————————— ball ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” ball â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     float ball_vertices[] = {
         -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
         -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f
@@ -393,14 +379,14 @@ void render() {
     glDisableVertexAttribArray(g_ball_program.positionAttribute);
     glDisableVertexAttribArray(g_ball_program.texCoordAttribute);
 
-    // ———————————————— GENERAL ———————————————— //
+    // â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” GENERAL â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€” //
     SDL_GL_SwapWindow(g_display_window);
 }
 
 void shutdown() { SDL_Quit(); }
 
 /**
- Start here—we can see the general structure of a game loop without worrying too much about the details yet.
+ Start here-Â—we can see the general structure of a game loop without worrying too much about the details yet.
  */
 int main(int argc, char* argv[])
 {
@@ -416,4 +402,3 @@ int main(int argc, char* argv[])
     shutdown();
     return 0;
 }
-
